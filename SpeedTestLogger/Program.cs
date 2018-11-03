8aa891836fbf65ed4e7bdaa7e1adaf9cbb58fa27
Alexander Hoem Rosbach <alexander.hoem.rosbach@computas.com>
2018-11-03 15:02:18 +0100

    Implement SpeedTestLogger

---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SpeedTest;
+using SpeedTest.Models;
+using System.Linq;
+using System.Globalization;
+using SpeedTestLogger.Models;
+
+namespace SpeedTestLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SpeedTestLogger!");
+
+            var config = new LoggerConfiguration();
+            var runner = new SpeedTestRunner(config.LoggerLocation);
+            var testData = runner.RunSpeedTest();
+            
+            var results = new TestResult
+            {
+                SessionId = new Guid(),
+                User = "alexander@rosbach.no",
+                Device = 1,
+                Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Data = testData
+            };
+        }
+    }
+}
